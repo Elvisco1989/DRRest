019a1e65a337872a9c95928a0e65a25ae83a2e8f
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-04-24 14:35:49 +0200

    Add id

--- DRRest/Music.cs
+++ DRRest/Music.cs
@@ -2,7 +2,7 @@
 {
     public class Music
     {    
-        public int Id;
+        public int Id { get; set; }
         public  string? Title  { get; set; }
 
         public string? Artist { get; set; }
