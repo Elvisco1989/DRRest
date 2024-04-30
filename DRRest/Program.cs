42c5a93538568f72e04f11b7b9e85f40f3ee8d5e
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-04-30 12:06:33 +0200

    Added Cors

--- DRRest/Program.cs
+++ DRRest/Program.cs
@@ -3,6 +3,13 @@ using DRRest;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+
+}));
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<MusicRepository>();
@@ -10,6 +17,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseCors("MyPolicy");
+
 
 // Configure the HTTP request pipeline.
 
