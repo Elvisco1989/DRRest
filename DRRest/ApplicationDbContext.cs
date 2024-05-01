a697e36ff9d626c55e572b50fc635588a95832df
Mbah Elvis Ndum <130441973+Elvisco1989@users.noreply.github.com>
2024-05-01 22:04:38 +0200

    Add dbcontext class

---
+++ DRRest/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DRRest
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        DbSet<Music> music { get; set; } 
+    }
+}
